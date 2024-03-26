bc4c63cccb6fea51fdbd4439d23f09f0adeb6843
bri.rhm@gmail.com <brheaume@c2r4p6.42quebec.com>
2024-03-26 18:14:45 -0400

    added canonical form

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -3,11 +3,19 @@
 
 # include <iostream>
 # include <string>
+# include <list>
+# include <map>
 
 class BitcoinExchange
 {
 	private:
 	public:
+		BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange& copy);
+		BitcoinExchange& operator=(const BitcoinExchange& copy);
+		~BitcoinExchange();
+
+
 };
 
 #endif
---
+++ cpp09/ex00/main.cpp
@@ -0,0 +1,11 @@
+#include "BitcoinExchange.hpp"
+
+int main()
+{
+	std::list<std::string> list;
+	std::map<std::string, int> map;
+
+	std::string line;
+	std::getline(line, "data.csv");
+	std::cout << line << std::endl;
+}
