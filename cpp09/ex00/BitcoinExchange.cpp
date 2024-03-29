4db3b402e1c129d2bbde4700654a8ae93f3dcfff
bri.rhm@gmail.com <brheaume@c2r6p4.42quebec.com>
2024-03-29 18:47:08 -0400

    almost finished soon(tm)

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -1,1 +1,37 @@
 #include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange()
+{
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& copy)
+{
+	(*this) = copy;
+}
+
+BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& copy)
+{
+	this->map = copy.map;
+	return (*this);
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+}
+
+void BitcoinExchange::insertValue(std::string src)
+{
+	size_t sepIndex = src.find(',');
+	std::string date;
+	float value;
+
+	date = src.substr(0, sepIndex);
+	value = static_cast<float>(std::stod(src.substr(sepIndex + 1, src.length() - sepIndex)));
+	this->map.insert(std::pair<std::string, float>(date, value));
+}
+
+float BitcoinExchange::btcValueAtDate()
+{
+	float value = 0.0f;
+	return (value);
+}
--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -11,6 +11,27 @@ bool validDateFormat(std::string src, size_t end)
 		else
 			return (false);
 	}
+	std::map<int, int> dayPerMonth;
+	int month = std::stoi(src.substr(YEAR_MONTH_SEPERATOR, YEAR_MONTH_SEPERATOR - MONTH_DAY_SEPERATOR));
+	int day = std::stoi(src.substr(MONTH_DAY_SEPERATOR, MONTH_DAY_SEPERATOR - src.length()));
+	int year = std::stoi(src.substr(0, YEAR_MONTH_SEPERATOR)); 
+	dayPerMonth[1] = 31;
+	if (year % 4)
+		dayPerMonth[2] = 28;
+	else
+		dayPerMonth[2] = 29;
+	dayPerMonth[3] = 31;
+	dayPerMonth[4] = 30;
+	dayPerMonth[5] = 31;
+	dayPerMonth[6] = 30;
+	dayPerMonth[7] = 31;
+	dayPerMonth[8] = 31;
+	dayPerMonth[9] = 30;
+	dayPerMonth[10] = 31;
+	dayPerMonth[11] = 30;
+	dayPerMonth[12] = 31;
+	if (day < 1 || month < 1 || month > 12 || year < 1 || day > dayPerMonth[month])
+		return (false);
 	return (true);
 }
 
@@ -22,10 +43,9 @@ void parseInput(std::string src)
 		if (pos == DATE_VALUE_SEPERATOR)
 		{
 			if (pos != src.length() - 1)
-				static_cast<float>(std::stod(src.c_str() + pos + 1));
+				(void)static_cast<float>(std::stod(src.c_str() + pos + 1));
 			if (!validDateFormat(src, pos - 1))
-				throw std::invalid_argument("invalid input syntax\ncorrect syntax:\"YYYY-MM-DD | [0-1000]\"");
-				
+				throw std::invalid_argument("invalid date format");
 		}
 	}
 	else
@@ -36,11 +56,11 @@ int main(int ac, char** av)
 {
 	if (ac == 2)
 	{
-		std::list<std::string> list;
+		//std::list<std::string> list;
 
 		std::string line;
 		std::fstream data("./data.csv", std::ios::in);
-		BitcoinExchange btc();
+		BitcoinExchange btc;
 		if (data.fail())
 		{
 			std::cout << "no data given" << std::endl;
@@ -52,10 +72,12 @@ int main(int ac, char** av)
 		try
 		{
 			while (std::getline(request, line))
-				parseInput(line, list);
+				parseInput(line);
 		}
 		catch (std::exception& e)
+		{
 			std::cout << e.what() << std::endl;
+		}
 	}
 	else 
 		std::cout << "invalid arguments" << std::endl;
