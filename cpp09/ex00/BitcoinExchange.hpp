06ccefe95de447bff3cd0e065ca1812fb42bcda3
sha0 <sha0@localhost.localdomain>
2024-03-28 22:05:32 -0400

    added parsing

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -1,21 +1,29 @@
 #ifndef BTCEXCHANGE_HPP_
 # define BTCEXCHANGE_HPP_
 
+# include <exception>
 # include <iostream>
+# include <fstream>
 # include <string>
 # include <list>
 # include <map>
 
+# define DATE_VALUE_SEPERATOR 11
+# define YEAR_MONTH_SEPERATOR 4
+# define MONTH_DAY_SEPERATOR 7
+
 class BitcoinExchange
 {
 	private:
+		std::map<std::string, float> map;
 	public:
 		BitcoinExchange();
 		BitcoinExchange(const BitcoinExchange& copy);
 		BitcoinExchange& operator=(const BitcoinExchange& copy);
 		~BitcoinExchange();
-
-
+		
+		void insertValue(std::string src);
+		float btcValueAtDate();
 };
 
 #endif
--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -1,11 +1,63 @@
 #include "BitcoinExchange.hpp"
 
-int main()
+bool validDateFormat(std::string src, size_t end)
 {
-	std::list<std::string> list;
-	std::map<std::string, int> map;
+	for (size_t i = 0; i < end; i++)
+	{
+		if (std::isdigit(src[i]))
+			continue;
+		if (src[i] == '-' && (i == YEAR_MONTH_SEPERATOR || i == MONTH_DAY_SEPERATOR))
+			continue;
+		else
+			return (false);
+	}
+	return (true);
+}
 
-	std::string line;
-	std::getline(line, "data.csv");
-	std::cout << line << std::endl;
+void parseInput(std::string src)
+{
+	size_t pos = src.find('|');
+	if (pos != std::string::npos)
+	{
+		if (pos == DATE_VALUE_SEPERATOR)
+		{
+			if (pos != src.length() - 1)
+				static_cast<float>(std::stod(src.c_str() + pos + 1));
+			if (!validDateFormat(src, pos - 1))
+				throw std::invalid_argument("invalid input syntax\ncorrect syntax:\"YYYY-MM-DD | [0-1000]\"");
+				
+		}
+	}
+	else
+		throw std::invalid_argument("invalid input syntax\ncorrect syntax:\"YYYY-MM-DD | [0-1000]\"");
+}
+
+int main(int ac, char** av)
+{
+	if (ac == 2)
+	{
+		std::list<std::string> list;
+
+		std::string line;
+		std::fstream data("./data.csv", std::ios::in);
+		BitcoinExchange btc();
+		if (data.fail())
+		{
+			std::cout << "no data given" << std::endl;
+			return (1);
+		}
+		while (std::getline(data, line))
+			std::cout << line << std::endl;
+		std::fstream request(av[1], std::ios::in);
+		try
+		{
+			while (std::getline(request, line))
+				parseInput(line, list);
+		}
+		catch (std::exception& e)
+			std::cout << e.what() << std::endl;
+	}
+	else 
+		std::cout << "invalid arguments" << std::endl;
+	return (0);
 }
