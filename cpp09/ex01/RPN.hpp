cf894d5718d4fd03da23c3266e96d442f72cadcc
bri.rhm@gmail.com <brheaume@c2r5p3.42quebec.com>
2024-04-04 19:03:03 -0400

    added RPN definitions & added arguments check

--- cpp09/ex01/RPN.hpp
+++ cpp09/ex01/RPN.hpp
@@ -1,13 +1,21 @@
 #ifndef RPN_HPP_
 # define RPN_HPP_
 
+# include <stack>
 # include <string>
 # include <iostream>
 
 class RPN
 {
 	private:
+		std::stack<std::string> stack;
 	public:
+		RPN();
+		RPN(const RPN& copy);
+		RPN& operator=(const RPN& copy);
+		~RPN();
+
+		void calculate(std::string);
 };
 
 #endif
--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -1,10 +1,32 @@
-#include "RPN.cpp"
+#include "RPN.hpp"
 
-bool check_arg(std::string)
+bool checkArg(std::string src)
 {
+	int nbOperators = 0;
+	int nbNumbers = 0;
+
+	for (size_t i = 0; i < src.length(); i++)
+	{
+		if (src[i] == '+' || src[i] == '-' || src[i] == '*' || src[i] == '/')
+			nbOperators++;
+		else if (std::isdigit(src[i]))
+			nbNumbers++;
+	}
+	if (nbOperators != nbNumbers - 1)
+		return (false);
+	return (true);
 }
 
 int main(int ac, char** av)
 {
-	
+	if (ac == 2)
+	{
+		RPN calc;
+		if (checkArg(av[1]))
+			calc.calculate(av[1]);
+		else
+			std::cout << "invalid amount of operators or numbers in the equation" << std::endl;
+	}
+	else
+		std::cout << "requires an equation" << std::endl;
 }
